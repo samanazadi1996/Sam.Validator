41fd2d5fd7f36b731f8e0c1d624e0b47adef92f9
saman azadi <samanazadi1996@gmail.com>
2025-05-11 10:42:32 +0330

    'UserController.cs' was modified and 'UserDto.cs' was added and 'Validator.cs' was modified
    Modified : Source/Sam.Validator.Api/Controllers/UserController.cs
    Added : Source/Sam.Validator.Api/Models/UserDto.cs
    Modified : Source/Sam.Validator/Validator.cs

--- Source/Sam.Validator.Api/Controllers/UserController.cs
+++ Source/Sam.Validator.Api/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Sam.Validator.Api.Models;
 
 namespace Sam.Validator.Api.Controllers
 {
@@ -10,48 +11,6 @@ namespace Sam.Validator.Api.Controllers
         public IActionResult Create([FromBody] UserDto data)
         {
             return Ok("Valid!");
-        }
-    }
-    public class UserDto : Validator<UserDto>
-    {
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? Role { get; set; }
-        public int Age { get; set; }
-        public string? Password { get; set; }
-        public DateTime BirthDate { get; set; }
-
-        public override void Validate()
-        {
-            RuleFor(x => x.Username)
-                .NotNull()
-                .NotEmpty()
-                .Length(3, 20)
-                .Matches(@"^[a-zA-Z0-9_]+$"); // Only letters, digits, underscores
-
-            RuleFor(x => x.Email)
-                .NotNull()
-                .NotEmpty()
-                .Email();
-
-            RuleFor(x => x.Role)
-                .NotNull()
-                .In("Admin", "User", "Guest");
-
-            RuleFor(x => x.Age)
-                .GreaterThan(17)
-                .LessThan(100)
-                .Min(18)
-                .Max(99);
-
-            RuleFor(x => x.Password)
-                .NotNull()
-                .Length(8, 100)
-                .Matches(@"[A-Z]").Must(x => x.Password!.Any(char.IsUpper), "Password must contain at least one uppercase letter.")
-                .Must(x => x.Password!.Any(char.IsDigit), "Password must contain at least one digit.");
-
-            RuleFor(x => x.BirthDate)
-                .Must(x => x.BirthDate < DateTime.Now, "BirthDate must be in the past.");
         }
     }
 
--- Source/Sam.Validator/Validator.cs
+++ Source/Sam.Validator/Validator.cs
@@ -2,101 +2,116 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 namespace Sam.Validator
 {
     public abstract class Validator<T> : IValidatableObject
     {
-        private readonly ICollection<ValidationResult> result = new List<ValidationResult>();
-        private Func<T, object?>? context;
-
+        private Dictionary<string, List<string>> Errors;
+        private object Value;
         public abstract void Validate();
 
-        protected T RuleFor(Func<T, object?> context)
+        protected T RuleFor(Expression<Func<T, object?>> expression)
         {
-            this.context = context;
+            Errors ??= new Dictionary<string, List<string>>();
 
-            if (context == null)
-                result.Add(new ValidationResult("Validation context is not set."));
+            string? fieldName = null;
+
+            if (expression.Body is MemberExpression member)
+                fieldName = member.Member.Name;
+
+            if (expression.Body is UnaryExpression unary && unary.Operand is MemberExpression memberOperand)
+                fieldName = memberOperand.Member.Name;
+
+            if (expression == null)
+                throw new ArgumentNullException(nameof(expression), "Validation expression is not set.");
+
+            if (fieldName != null)
+            {
+                Errors.TryAdd(fieldName, new List<string>());
+                Errors = Errors.OrderBy(p => p.Key == fieldName).ToDictionary(p => p.Key, x => x.Value);
+            }
+
+            Value = expression.Compile()((T)(object)this!);
 
             return (T)(object)this!;
         }
 
         protected T NotNull()
         {
-            var value = context?.Invoke((T)(object)this!);
-            if (value is null)
-                result.Add(new ValidationResult("Value cannot be null."));
+            if (Value is null)
+                AddError("Value cannot be null.");
+
             return (T)(object)this!;
         }
 
         protected T NotEmpty()
         {
-            var value = context?.Invoke((T)(object)this!);
-            if (string.IsNullOrWhiteSpace(value?.ToString()))
-                result.Add(new ValidationResult("Value cannot be empty."));
+            if (string.IsNullOrWhiteSpace(Value?.ToString()))
+                AddError("Value cannot be empty.");
+
             return (T)(object)this!;
         }
 
         protected T Min(int min)
         {
-            if (int.TryParse(context?.Invoke((T)(object)this!)?.ToString(), out var value) && value < min)
-                result.Add(new ValidationResult($"Minimum allowed value is {min}."));
+            if (int.TryParse(Value?.ToString(), out var value) && value < min)
+                AddError($"Minimum allowed value is {min}.");
             return (T)(object)this!;
         }
 
         protected T Max(int max)
         {
-            if (int.TryParse(context?.Invoke((T)(object)this!)?.ToString(), out var value) && value > max)
-                result.Add(new ValidationResult($"Maximum allowed value is {max}."));
+            if (int.TryParse(Value?.ToString(), out var value) && value > max)
+                AddError($"Maximum allowed value is {max}.");
             return (T)(object)this!;
         }
 
         protected T Length(int minLength, int maxLength)
         {
-            var value = context?.Invoke((T)(object)this!)?.ToString();
+            var value = Value?.ToString();
             if (value != null && (value.Length < minLength || value.Length > maxLength))
-                result.Add(new ValidationResult($"Value must be between {minLength} and {maxLength} characters."));
+                AddError($"Value must be between {minLength} and {maxLength} characters.");
             return (T)(object)this!;
         }
 
         protected T Matches(string pattern)
         {
-            var value = context?.Invoke((T)(object)this!)?.ToString();
+            var value = Value?.ToString();
             if (value != null && !Regex.IsMatch(value, pattern))
-                result.Add(new ValidationResult($"Value does not match the required pattern."));
+                AddError($"Value does not match the required pattern.");
             return (T)(object)this!;
         }
 
         protected T Must(Func<bool> condition)
         {
             if (!condition())
-                result.Add(new ValidationResult("Custom validation failed."));
+                AddError("Custom validation failed.");
             return (T)(object)this!;
         }
 
         protected T Email()
         {
-            var value = context?.Invoke((T)(object)this!)?.ToString();
+            var value = Value?.ToString();
             if (!string.IsNullOrWhiteSpace(value) && !Regex.IsMatch(value, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
-                result.Add(new ValidationResult("Invalid email format."));
+                AddError("Invalid email format.");
             return (T)(object)this!;
         }
 
         protected T In(params string[] allowed)
         {
-            var value = context?.Invoke((T)(object)this!)?.ToString();
+            var value = Value?.ToString();
             if (!string.IsNullOrWhiteSpace(value) && !allowed.Contains(value))
-                result.Add(new ValidationResult($"Value must be one of the following: {string.Join(", ", allowed)}"));
+                AddError($"Value must be one of the following: {string.Join(", ", allowed)}");
             return (T)(object)this!;
         }
 
         protected T GreaterThan<TValue>(TValue min) where TValue : IComparable
         {
-            var value = context?.Invoke((T)(object)this!);
-            if (value is TValue comparable && comparable.CompareTo(min) <= 0)
-                result.Add(new ValidationResult($"Value must be greater than {min}."));
+            if (Value is TValue comparable && comparable.CompareTo(min) <= 0)
+                AddError($"Value must be greater than {min}.");
             return (T)(object)this!;
         }
 
@@ -104,7 +119,7 @@ namespace Sam.Validator
         {
             if (!predicate((T)(object)this!))
             {
-                result.Add(new ValidationResult(errorMessage ?? "Custom condition failed."));
+                AddError(errorMessage ?? "Custom condition failed.");
             }
 
             return (T)(object)this!;
@@ -112,17 +127,22 @@ namespace Sam.Validator
 
         protected T LessThan<TValue>(TValue max) where TValue : IComparable
         {
-            var value = context?.Invoke((T)(object)this!);
-            if (value is TValue comparable && comparable.CompareTo(max) >= 0)
-                result.Add(new ValidationResult($"Value must be less than {max}."));
+            if (Value is TValue comparable && comparable.CompareTo(max) >= 0)
+                AddError($"Value must be less than {max}.");
             return (T)(object)this!;
         }
 
+
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            result.Clear();
             Validate();
-            return result;
+
+            return Errors.SelectMany(p => p.Value.Distinct().Select(x => new ValidationResult(x, new string[] { p.Key })));
+        }
+
+        void AddError(string message)
+        {
+            Errors.LastOrDefault().Value.Add(message);
         }
     }
 
